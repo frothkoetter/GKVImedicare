75124d23071e794f90bc2740b7c3b5653742b5e6
Friedel <frothkoetter@cloudera.com>
2019-12-05 17:01:54 +0000

    sieht gut au

--- predict_strittig.py
+++ predict_strittig.py
@@ -1,21 +1,23 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.ml import PipelineModel
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
 
 spark = SparkSession.builder \
       .appName("Telco Customer Churn") \
       .master("local[*]") \
       .getOrCreate()
   
-model = PipelineModel.load("file:///home/cdsw/models/spark") 
+model = PipelineModel.load("file:///home/cdsw/models/cms_model_spark") 
 
-features = ["intl_plan", "account_length", "number_vmail_messages", "total_day_calls",
-                        "total_day_charge", "total_eve_calls", "total_eve_charge",
-                        "total_night_calls", "total_night_charge", "total_intl_calls", 
-                        "total_intl_charge","number_customer_service_calls"]
+
+features = ["fachgebiet", "krankenhaus_oder_arzt", "medikament", 
+                       "bundesland", "auszahlung_monat", "auszahlung_euro" ]
 
 def predict(args):
-  account=args["feature"].split(",")
-  feature = spark.createDataFrame([account[:1] + map(float,account[1:12])], features)
-  result=model.transform(feature).collect()[0].prediction
-  return {"result" : result}
+    account=args["feature"].split(",")
+    feature = spark.createDataFrame([account[:1] + list(map(float,account[1:6]))], features)
+    
+    result=model.transform(feature).collect()[0].prediction
+    return {"result" : result}
